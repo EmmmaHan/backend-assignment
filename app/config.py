70fab7c816f3a57b3c5dbef57c5d760bde408a9f
emmmahan <emma_han@berkeley.edu>
2023-05-19 14:01:18 +0900

    add test db

--- app/config.py
+++ app/config.py
@@ -10,3 +10,21 @@ def get_db_uri():
     os.getenv("db_port","3306"),
     os.getenv("db_name","bering_assignment"),
 )
+
+
+def get_test_db_uri():
+    return "{}://{}:{}@{}:{}/{}".format(
+    os.getenv("db_protocol","mysql+mysqlconnector"),
+    os.getenv("db_user","root"),
+    os.getenv("db_password","ffbb"),
+    os.getenv("db_host","localhost"),
+    os.getenv("db_port","3306"),
+    os.getenv("db_name","bering_assignment_test"),
+)
+
+def get_sys_db_uri():
+    if os.getenv("env","") == "test":
+        return get_test_db_uri()
+    else:
+        return get_db_uri()
+

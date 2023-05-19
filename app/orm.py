cbe5f994c9761d2a1c25da7c9affe1e5caa2f468
emmmahan <emma_han@berkeley.edu>
2023-05-19 14:00:25 +0900

    fix method to return value and clean up

--- app/orm.py
+++ app/orm.py
@@ -4,11 +4,13 @@ from db import engine, UserCard, UserAccount
 from sqlalchemy.exc import IntegrityError, NoResultFound
 from typing import List
 
-def create_object(object: object) -> None:
+def create_object(object: object) -> int:
     try:
         with Session(engine) as session:
             session.add(object)
             session.commit()
+            session.refresh(object)
+            return object.id
     except IntegrityError as e:
         raise e
     except SQLAlchemyError as e:
@@ -31,17 +33,17 @@ def get_object_by_id(id: int, klass: object) -> object:
             q=session.query(klass).filter(klass.id==id)
             obj = q.one()
             return obj
+    except NoResultFound as e:
+        raise e
     except IntegrityError as e:
         raise e
     except SQLAlchemyError as e:
         raise e
 
-def update_card_status(card_id: int, status: str) -> None:
+def update_card_status(card: UserCard) -> None:
     try:
         with Session(engine) as session:
-            q = session.query(UserCard).filter(UserCard.id==card_id)
-            card = q.one()
-            card.status = status
+            card
             session.commit()
     except NoResultFound as e:
         raise e
@@ -64,20 +66,5 @@ def update_account_balance(account_id: int, balance: int):
             raise e
         except SQLAlchemyError as e:
             raise e
-        
-
-        
-# def check_user_password(user_id, password):
-#     try:    
-#         with Session(engine) as session:
-#             q = session.query(User).filter(User.id==user_id)
-#             user = q.one()
-#             if not user.verify_password(password):
-#                 raise NoResultFound
-#             return True
-#     except IntegrityError as e:
-#         raise IndentationError
-#     except SQLAlchemyError as e:
-#         raise e
 
 
